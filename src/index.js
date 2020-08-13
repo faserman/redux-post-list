7d22d8c26ee574819bd70c2c3c0174e2c829f3b6
Nikolay <45463838+faserman@users.noreply.github.com>
2020-08-14 01:52:42 +0500

    release-v0.1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { render } from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux'; 
+import { rootReducer } from './redux/rootReducer';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(
-  <React.StrictMode>
+const store = createStore(rootReducer);
+
+const app = (
+  <Provider store={store}>
     <App />
+  </Provider>
+)
+
+render(
+  <React.StrictMode>
+    { app }
   </React.StrictMode>,
   document.getElementById('root')
 );
